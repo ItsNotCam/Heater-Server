c9613cb5f4013efd54c9e7865f48bef0f139cb0c
ItsNotCam <camallanyoung@gmail.com>
2023-10-19 01:02:49 -0400

    commented startup script

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
+# Get interface name for IP
 if [ "$1" = "" ]
 then
   echo "Usage: $0 <interface name>"
   exit
 fi;
 
+# Install TMUX to run the app
 sudo apt install tmux
 
+# Save the current working directory
 CWD=$(pwd)
+
+# Get IP and set the IP config file in the React App
 cd $CWD/app/client
 
 IP=$(ip -f inet addr show $1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
@@ -15,7 +20,10 @@ echo "
 export const IP: string = \"$IP\";
 " > IP.ts;
 
+# Install NPM dependencies
 npm install --force
+
+# Build and move built files to a 'static' folder within the web server's data
 npm run build
 
 if [ ! -d "$CWD/app/server/static" ];
@@ -25,12 +33,15 @@ fi
 
 mv build/* $CWD/app/server/static
 
+# Return to starting directory
 cd $CWD
 
+# Install python dependencies in a virtual environment
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
+# Kill TMUX windows running the heater app
 if [ $(tmux list-sessions | grep "heaterpi-web") ];
 then
   tmux kill-session -t heaterpi-web
@@ -41,5 +52,6 @@ then
   tmux kill-session -t heaterpi-socket
 fi
 
+# Start servers in new TMUX sessions
 tmux new-session -d -s heaterpi-web python3 app/server/WebServer.py
 tmux new-session -d -s heaterpi-web python3 app/server/ThermostatServer.py
