b6a6a20bd6ed6ec8c176d83c740c1786f158536a
ItsNotCam <camallanyoung@gmail.com>
2023-10-19 14:34:29 +0100

    Release 1

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -2,4 +2,4 @@ from .HueControl import HueControl
 from .Logger import *
 from .TemperatureData import TemperatureData
 from .TemperatureReader import read_temperature
-from .ThermostatServer import ThermostatServer
+# from .ThermostatServer import ThermostatServer
