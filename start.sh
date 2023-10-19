b80070b0c33aa1e4c1e0dbe9745d8f4a573578e5
ItsNotCam <camallanyoung@gmail.com>
2023-10-19 15:35:50 +0100

    startup and stop scripts

--- start.sh
+++ start.sh
@@ -1,18 +1,33 @@
+GREEN='\033[1;32m'
+WHITE='\033[1;37m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+
 cd app
 
-# Install Dependencies
+#Install Dependencies
+echo -e "${GREEN}Installing Dependencies${WHITE}"
 sudo apt install tmux python3 python3-pip
 pip3 install -r requirements.txt
 
+echo ""
+
 # Kill TMUX windows running the heater app
 WINDOW_NAME="heaterpi"
-
-if [ $(tmux list-sessions | grep "$WINDOW_NAME") ];
+SESSION=$(tmux list-session | grep $WINDOW_NAME)
+if [ "$SESSION" ]
 then
+  echo -e "${YELLOW}Killing any old HeaterPi instances"
   tmux kill-session -t $WINDOW_NAME
 fi
 
 # Start servers in new TMUX sessions
+echo -e "${CYAN}Starting up HeaterPi in TMUX..."
 tmux new-session -d -s $WINDOW_NAME python3 WebServer.py
 tmux split-window -h -t $WINDOW_NAME
 tmux send-keys -t $WINDOW_NAME "python3 SocketServer.py" C-m
+
+echo ""
+
+echo -e "${GREEN}All done!${WHITE}"
+echo "Just type \"tmux attach -t heaterpi\" to view the running app!" 
